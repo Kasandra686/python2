55b0fd334f93d5a04a3204aeddad83e9ed7bf0c5
Вера <verazverev@yandex.ru>
2020-01-18 21:22:52 +0500

    сделано 3 задание

--- 2.py
+++ 2.py
@@ -5,7 +5,7 @@ for el in sp:
     i += 1
     if (i % 2) != 0:
         el2 = sp[i - 1]
-        if (len(sp) == i):
+        if len(sp) == i:
             sp2.append(el2)
     else:
         sp2.append(el)
