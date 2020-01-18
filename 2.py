084662b993c67bbc68f5c6bb8086451d790bc29f
Вера <verazverev@yandex.ru>
2020-01-18 20:48:30 +0500

    сделано 2 задание

---
+++ 2.py
@@ -0,0 +1,13 @@
+sp = input("Введите список")
+sp2 = []
+i = 0
+for el in sp:
+    i += 1
+    if (i % 2) != 0:
+        el2 = sp[i - 1]
+        if (len(sp) == i):
+            sp2.append(el2)
+    else:
+        sp2.append(el)
+        sp2.append(el2)
+print(sp2)
