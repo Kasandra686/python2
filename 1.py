18538b1333562b17ec3fc32b50692110fd4101bb
Вера <verazverev@yandex.ru>
2020-01-18 19:53:29 +0500

    сделано первое задание

---
+++ 1.py
@@ -0,0 +1,4 @@
+sp = [6, 'Python', 3.5, {1, 2, 3}, ['Саша', 'Ива', 'Витя'], (5, 9, 1), {'name': 'Vera', 'soname': 'Zvereva'}, True, b'text', bytearray(b'6666'), None ]
+print (sp)
+for i in sp:
+    print(type(i))
